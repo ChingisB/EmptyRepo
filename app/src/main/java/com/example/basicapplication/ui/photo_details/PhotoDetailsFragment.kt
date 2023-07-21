64b19ae69d705c5a9883a479e8960bb6ecb85be2
ChingisB <c_bogdatov@kbtu.kz>
2023-07-21 14:50:01 +0600

    Like button animation

--- app/src/main/java/com/example/basicapplication/ui/photo_details/PhotoDetailsFragment.kt
+++ app/src/main/java/com/example/basicapplication/ui/photo_details/PhotoDetailsFragment.kt
@@ -1,12 +1,14 @@
 package com.example.basicapplication.ui.photo_details
 
 
+import android.animation.ValueAnimator
 import android.content.Context
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import com.bumptech.glide.Glide
 import com.example.base.BaseFragment
 import com.example.basicapplication.MainApplication
+import com.example.basicapplication.R
 import com.example.basicapplication.SharedPhotoViewModel
 import com.example.basicapplication.dagger.DaggerViewModelFactory
 import com.example.basicapplication.databinding.FragmentPhotoDetailsBinding
@@ -18,7 +20,8 @@ import javax.inject.Inject
 
 class PhotoDetailsFragment : BaseFragment<FragmentPhotoDetailsBinding, PhotoDetailsViewModel>() {
 
-    @Inject lateinit var viewModelFactory: DaggerViewModelFactory
+    @Inject
+    lateinit var viewModelFactory: DaggerViewModelFactory
     override val viewModel: PhotoDetailsViewModel by viewModels { viewModelFactory }
     private val sharedPhotoViewModel: SharedPhotoViewModel by activityViewModels()
     private lateinit var photoEntity: PhotoEntity
@@ -34,10 +37,21 @@ class PhotoDetailsFragment : BaseFragment<FragmentPhotoDetailsBinding, PhotoDeta
     override fun setupListeners() {
         super.setupListeners()
         binding.backButton.setOnClickListener { parentFragmentManager.popBackStack() }
-        binding.root.setOnClickListener{}
+        binding.root.setOnClickListener {}
         binding.saveButton.setOnClickListener {
-            if(photoEntity.isSaved) viewModel.removePhoto(photoEntity)
-            else viewModel.savePhoto(photoEntity)
+            val greyColor = requireContext().getColor(R.color.grey)
+            val pinkColor = requireContext().getColor(R.color.dark_pink)
+            if (photoEntity.isSaved) {
+                viewModel.removePhoto(photoEntity)
+                ValueAnimator.ofArgb(greyColor, pinkColor)
+            } else {
+                viewModel.savePhoto(photoEntity)
+                ValueAnimator.ofArgb(pinkColor, greyColor)
+            }.apply {
+                duration = LIKE_ANIMATION_DURATION
+                start()
+                addUpdateListener { binding.saveButton.background.setTint(it.getAnimatedValue() as Int)}
+            }
         }
     }
 
@@ -50,14 +64,18 @@ class PhotoDetailsFragment : BaseFragment<FragmentPhotoDetailsBinding, PhotoDeta
             binding.saveButton.isChecked = it.isSaved
             Glide.with(binding.image).load(Config.MEDIA_URL + it.image.name).into(binding.image)
             viewModel.viewPhoto(it)
-            viewModel.getTotalViews(it.id){ totalViews ->
-                binding.totalViewsText.text = if(totalViews > 999) Constants.MAX_VIEWS else totalViews.toString()
+            viewModel.getTotalViews(it.id) { totalViews ->
+                binding.totalViewsText.text = if (totalViews > 999) Constants.MAX_VIEWS else totalViews.toString()
             }
         }
 
-        sharedPhotoViewModel.userLiveData.observe(viewLifecycleOwner){ binding.usernameText.text = it.username }
+        sharedPhotoViewModel.userLiveData.observe(viewLifecycleOwner) { binding.usernameText.text = it.username }
 
-        viewModel.photoSavedState.observe(viewLifecycleOwner){ binding.saveButton.isChecked = it }
+        viewModel.photoSavedState.observe(viewLifecycleOwner) { binding.saveButton.isChecked = it }
+    }
+
+    companion object{
+        const val LIKE_ANIMATION_DURATION = 200L
     }
 
 }
